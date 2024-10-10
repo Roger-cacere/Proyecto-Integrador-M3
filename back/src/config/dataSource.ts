e33fdcf0091b0cea4bec93b768f033b9294dd190
Roger-cacere <roger.ronald.caceres@gmail.com>
2024-10-10 14:46:45 -0300

    Ultimo retoque

--- back/src/config/dataSource.ts
+++ back/src/config/dataSource.ts
@@ -3,14 +3,15 @@ import { User } from "../entities/User";
 import { Appointment } from "../entities/Appointments";
 import { Credential } from "../entities/Credential";
 import { UserLogin } from "../entities/Login";
+import { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DB  } from "./envs";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "Roger.315",
-    database: "miproyecto", 
+    host: DB_HOST,
+    port: Number(DB_PORT),
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DB, 
     entities: [User, Appointment, Credential, UserLogin],
     synchronize: true,
     logging: false
